afbc9a142bea8554c23e186eb24f1ea9652ad35f
Matan Maron <38442407+matanmaron@users.noreply.github.com>
2021-09-15 13:09:11 +0300

    fix menu key

--- Assets/Flat Platformer Template/Scripts/Menu.cs
+++ Assets/Flat Platformer Template/Scripts/Menu.cs
@@ -5,20 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             Application.Quit();
         }
-        if (Input.anyKey)
+        else if (Input.GetKeyDown(KeyCode.Space))
         {
             SceneManager.LoadScene(1);
         }
